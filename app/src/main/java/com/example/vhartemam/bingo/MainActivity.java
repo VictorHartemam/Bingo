acfd858cc68e6e986e788bc593f3943da56313fb
hartemav <victor.harteman@hotmail.com>
2018-09-08 14:53:34 -0300

    Adding some alternative layout files

--- app/src/main/java/com/example/vhartemam/bingo/MainActivity.java
+++ app/src/main/java/com/example/vhartemam/bingo/MainActivity.java
@@ -81,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative_layout_activity_main);
 
         //Após criar a tela
         geraNumeroRandom = new Random(System.currentTimeMillis());
